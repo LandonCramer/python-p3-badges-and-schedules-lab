0a4909637cf34f4036815b256947c3edb28bd8cd
Landon <landoncramer1@gmail.com>
2024-06-02 11:06:14 -0700

    practice

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -2,19 +2,20 @@ def badge_maker(name):
     return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return [f'Hello, my name is {name}.' for name in names]
-
+    list_names = []
+    for name in names:
+        list_names.append(f"Hello, my name is {name}.")
+    return list_names
 def assign_rooms(names):
     rooms = range(1,9)
-
-    assignments = []
+    
+    room_assign = []
     for room in rooms:
-        assignments.append(f'Hello, {names[room - 1]}! You\'ll be assigned to room {room}!')
-    return assignments
+        room_assign.append(f"Hello, {names[room -1]}! You'll be assigned to room {room}!")
+    return room_assign    
 
 def printer(names):
     for badge in batch_badge_creator(names):
         print(badge)
-
-    for assignment in assign_rooms(names):
-        print(assignment)
+    for assign in assign_rooms(names):
+        print(assign)
